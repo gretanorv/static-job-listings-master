7298191d3dfc01f0a0aeb8523b2f225c8a420b83
gretanorv <gretanorv@gmail.com>
2020-06-02 14:39:19 +0300

    show/hide filter boxes

--- src/js/partials/_filter.js
+++ src/js/partials/_filter.js
@@ -3,7 +3,7 @@ const filterModal = document.querySelector(".filter-modal");
 const clearFilters = document.querySelector(".filter-modal__control-clear");
 const filterText = document.querySelector(".filters-content__data");
 const filterModalBox = document.querySelector(".filter-modal__box");
-const filterModalBoxAll = document.querySelectorAll(".filter-modal__box");
+var filterModalBoxAll = document.querySelectorAll(".filter-modal__box");
 const jobCards = document.querySelectorAll(".job-card");
 const filterWrapper = document.querySelector(".filter-wrapper");
 const filtersContent = document.querySelector(".filters-content");
@@ -13,31 +13,25 @@ var filterList = [];
 filterBox.forEach((element) => {
   element.addEventListener("click", () => {
     filterModal.classList.remove("hide");
-    addToFilterList(element);
     addFilterToModal(element.innerHTML);
     filterSelection();
   });
 });
 
-clearFilters.addEventListener("click", () => {
-  filterModal.classList.add("hide");
-  jobCards.forEach((card) => {
-    card.classList.remove("hide");
-  });
-});
-
 const addFilterToModal = (text) => {
-  if (filterList.length === 1) {
-    filterText.innerHTML = text;
-  } else {
-    var cloneFilterBox = filterModalBox.cloneNode(true);
-    cloneFilterBox.children[0].children[0].innerHTML = text;
-    filterWrapper.appendChild(cloneFilterBox);
+  if (!filterList.includes(text)) {
+    filterList.push(text);
+    if (filterList.length === 1) {
+      filterText.innerHTML = text;
+      filterModalBox.classList.remove("hide");
+    } else {
+      var cloneFilterBox = filterModalBox.cloneNode(true);
+      cloneFilterBox.children[0].children[0].innerHTML = text;
+      filterWrapper.appendChild(cloneFilterBox);
+    }
   }
-  var removeFilterButton = document.querySelectorAll(
-    ".filters-content__control"
-  );
-  console.log(removeFilterButton);
+
+  removeFilterButton = document.querySelectorAll(".filters-content__control");
 
   removeFilterButton.forEach((element) => {
     element.addEventListener("click", () => {
@@ -46,31 +40,28 @@ const addFilterToModal = (text) => {
   });
 };
 
-// removeFilterButton.addEventListener("click", () => {
-//   // filterModalBox.classList.add("hide");
-//   filterSelection(element.innerHTML);
-// });
-
-const addToFilterList = (item) => {
-  if (!filterList.includes(item)) {
-    filterList.push(item.innerHTML);
-  }
-};
-
-//
 function filterSelection() {
   for (i = 0; i < jobCards.length; i++) {
     jobCards[i].classList.add("hide");
 
-    filterList.every((filter) => {
-      if (
-        filterList.every((v) => Array.from(jobCards[i].classList).includes(v))
-      ) {
-        jobCards[i].classList.remove("hide");
-      }
-    });
+    if (
+      filterList.every((v) => Array.from(jobCards[i].classList).includes(v))
+    ) {
+      jobCards[i].classList.remove("hide");
+    }
   }
 }
+
+clearFilters.addEventListener("click", () => {
+  console.log("here");
+  filterModal.classList.add("hide");
+  filterList = [];
+  filterModalBoxAll = document.querySelectorAll(".filter-modal__box");
+  filterModalBoxAll.forEach((box) => box.classList.add("hide"));
+  jobCards.forEach((card) => {
+    card.classList.remove("hide");
+  });
+});
 
 function unFilterSelection(element) {
   console.log(element);
